8b7827d51bce43e16e2533b25ac3c81de03ce413
gahyeon <117976216+gahyeon11@users.noreply.github.com>
2022-11-25 17:19:21 +0900

    2

--- sketch.js
+++ sketch.js
@@ -11,7 +11,7 @@ function setup() {
   wave.amp(0.1); //amp 값 올리고 내릴 수 있음
 
   //이렇게 하면 실행 되지 않음 왜냐면 구글에서 실행하면서 영상이나 소리가 나오는 것을 못하게 막아놓았기 때문에
-  button=createButton("play/pause");
+  button=createButton('play/pause');
   button.mousePressed(toggle);
 }
 function draw(){
