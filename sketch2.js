b62913f5ee63d8b17663f054974fed24bb1a3e6c
gahyeon <117976216+gahyeon11@users.noreply.github.com>
2022-12-02 15:41:07 +0900

    Create sketch2.js
    2

---
+++ sketch2.js
@@ -0,0 +1,25 @@
+var wave;
+var playing=false;
+var button;
+function setup() {
+  //createCanvas(displayWidth, displayHeight);
+  createCanvas(640,480);
+  r = random(50,255);
+  g=random(0,100);
+  b=(50, 255);
+}
+function draw(){
+  background(r,g,b);
+  console.log('draw');
+
+}
+function deviceMoved(){
+  r=map(accelerationX, -90, 90, 100, 175);
+  g=map(accelerationY, -90, 90, 100, 200);
+  b=map(accelerationZ, -90, 90, 100, 200);
+}
+
+
+// function draw() {
+//   background(220);
+// }
