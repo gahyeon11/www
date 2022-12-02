e47ad68ecd0e13d64f87eb51d76676bdac00c9d1
gahyeon <117976216+gahyeon11@users.noreply.github.com>
2022-12-02 15:54:07 +0900

    Update sketch2.js

--- sketch2.js
+++ sketch2.js
@@ -1,8 +1,8 @@
 
 let r,g,b;
 function setup() {
-  //createCanvas(displayWidth, displayHeight);
-  createCanvas(640,480);
+  createCanvas(displayWidth, displayHeight);
+  //createCanvas(640,480);
   r = random(50,255);
   g=random(0,100);
   b=(50, 255);
@@ -10,7 +10,7 @@ function setup() {
 function draw(){
   background(r,g,b);
   //console.log('draw');
-
+  //text(accelerationX","+ accelerationY","+accelerationZ);
 }
 function deviceMoved(){
   r=map(accelerationX, -90, 90, 100, 175);
